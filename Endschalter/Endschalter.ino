a801b39d00943aa5af7ce37a9d83b4133d4c2720
DA-Tripod <mrgamestest@gmail.com>
2014-05-07 15:06:35 +0200

    Update Endschalter.ino

--- Endschalter/Endschalter.ino
+++ Endschalter/Endschalter.ino
@@ -1,95 +1,160 @@
+// positive Schritte -> im Uhrzeigersinn
+
+/* 
+ Stepper Motor Control - one revolution
+ 
+ This program drives a unipolar or bipolar stepper motor.
+ 
+ The motor driver is attached to digital pins 8,9 of the Arduino.
+ 
+ The motor should revolve one revolution in one direction, then
+ one revolution in the other direction.  
+ 
+ Changed by Ronivaldo Sampaio to M Series
+ 2013
+ https://github.com/ronivaldo/Arduino-StepperMSeries
+ 
+ Original:
+ http://www.arduino.cc/en/Reference/Stepper
+ This example code is in the public domain.
+ Created 11 Mar. 2007
+ Modified 30 Nov. 2009
+ by Tom Igoe
+ 
+ */
+
+#include <StepperMSeries.h>
+
+// change this to the number of steps on your motor
+#define STEPS 200
+#define PULSE_PIN_1 37
+#define DIRECTION_PIN_1 36
+#define PULSE_PIN_2 35
+#define DIRECTION_PIN_2 34
+#define PULSE_PIN_3 33
+#define DIRECTION_PIN_3 32
+
 #define input_1 2
 #define input_2 3
 #define input_3 4
-#define input_4 5
-#define input_5 6
-#define input_6 7
+#define input_12 5
+#define input_22 6
+#define input_32 7
 
 boolean reference_1 = false;
 boolean reference_2 = false;
 boolean reference_3 = false;
-boolean reference_4 = false;
-boolean reference_5 = false;
-boolean reference_6 = false;
+
+int angle_1=0;
+int angle_2=0;
+int angle_3=0;
+
+// create an instance of the stepper class, specifying
+// the number of steps of the motor and the pins it's
+// attached to
+StepperMSeries stepper_1(STEPS, PULSE_PIN_1, DIRECTION_PIN_1);
+StepperMSeries stepper_2(STEPS, PULSE_PIN_2, DIRECTION_PIN_2);
+StepperMSeries stepper_3(STEPS, PULSE_PIN_3, DIRECTION_PIN_3);
+
 
 void setup() {
+  
+  
   Serial.begin(9600);
-  delay(1000);
-  attachInterrupt(input_1, doReference_1, FALLING);
-  attachInterrupt(input_2, doReference_2, FALLING);
-  attachInterrupt(input_3, doReference_3, FALLING);
-  attachInterrupt(input_4, doReference_4, FALLING);
-  attachInterrupt(input_5, doReference_5, FALLING);
-  attachInterrupt(input_6, doReference_6, FALLING);
+  // set the speed at 60 rpm:
+  stepper_1.setSpeed(60);
+  stepper_2.setSpeed(60);
+  stepper_3.setSpeed(60);
+  
+  pinMode(input_1, INPUT);
+  pinMode(input_2, INPUT);
+  pinMode(input_3, INPUT);
+  pinMode(input_12, INPUT);
+  pinMode(input_22, INPUT);
+  pinMode(input_32, INPUT);
+  
   delay(1000);
 }
 
 void loop() {
-  // wenn Taster offen -> liefert HIGH am Eingang
-  if(!reference_1 || !reference_2 || !reference_3 || !reference_4 || !reference_5 || !reference_6){
+  
+  if(!reference_1 || !reference_2 || !reference_3){
     if(!reference_1){
-      Serial.println("Fahre hoch 1");
+      if(!digitalRead(input_1)){
+        reference_1=true;
+        angle_1=175;
+        stepper_1.step(2);
+        reference_1=true; 
+      }else{
+        Serial.println("Fahre hoch 1");
+        stepper_1.step(-1);
+      }
     }
     if(!reference_2){
-      Serial.println("Fahre hoch 2");
+      if(!digitalRead(input_2)){
+        reference_2=true;
+        angle_2=175;
+        stepper_2.step(2);
+        reference_2=true; 
+      }else{
+        stepper_2.step(-1);
+        Serial.println("Fahre hoch 2");
+      }
     }
-    if(!reference_3){
-      Serial.println("Fahre hoch 3");
+    if(!reference_3){ 
+      if(!digitalRead(input_3)){
+        Serial.println(digitalRead(input_3));
+        reference_3=true;
+        angle_3=175;
+        stepper_3.step(2);
+        reference_3=true; 
+      }else{
+        Serial.println("Fahre hoch 3");
+        stepper_3.step(-1);
+      }
     }
-  if(!reference_4){
-      Serial.println("Fahre hoch 4");
-    }
-    if(!reference_5){
-      Serial.println("Fahre hoch 5");
-    }
-    if(!reference_6){
-      Serial.println("Fahre hoch 6");
-    }  
   }else{
     Serial.println("Messen");
+    
+    doCheck();
   }
-  delay(100);  
-  Serial.println("");
+  delay(500); 
+  Serial.println(""); 
 }
 
-void doReference_1(){
-  if(!reference_1 && millis()>5000){
-    reference_1 = true;
+void doCheck(){
+  if(!digitalRead(input_1)){
+    Serial.println("Input_1");
+    stepper_1.step(3);
+    angle_1=175;
   }
-  Serial.println("Fahre runter 1");
+  else if(!digitalRead(input_12)){
+    Serial.println("Input_12");
+    stepper_1.step(-3);
+    angle_1=95;
+  }
+  
+  if(!digitalRead(input_2)){
+    Serial.println("Input_2");
+    stepper_2.step(3);
+    angle_2=175;
+  }
+  else if(!digitalRead(input_22)){
+    Serial.println("Input_22");
+    stepper_2.step(-3);
+    angle_2=95;
+  }
+  
+  if(!digitalRead(input_3)){
+    Serial.println("Input_3");
+    stepper_3.step(3);
+    angle_3=175;
+  }
+  else if(!digitalRead(input_32)){
+    Serial.println("Input_32");
+    stepper_3.step(-3);
+    angle_3=95;
+  }
 }
 
-void doReference_2(){
-  if(!reference_2 && millis()>5000){
-    reference_2 = true;
-  }
-  Serial.println("Fahre runter 2");
-}
 
-void doReference_3(){
-  if(!reference_3 && millis()>5000){
-    reference_3 = true;
-  }
-  Serial.println("Fahre runter 3");
-}
-
-void doReference_4(){
-  if(!reference_4 && millis()>5000){
-    reference_4 = true;
-  }
-  Serial.println("Fahre runter 3");
-}
-
-void doReference_5(){
-  if(!reference_5 && millis()>5000){
-    reference_5 = true;
-  }
-  Serial.println("Fahre runter 3");
-}
-
-void doReference_6(){
-  if(!reference_6 && millis()>5000){
-    reference_6 = true;
-  }
-  Serial.println("Fahre runter 3");
-}
